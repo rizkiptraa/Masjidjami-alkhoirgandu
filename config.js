d530210f8e495f6aa0e666d7be6bffb699beb300
rizkiptraa <110907538+rizkiptraa@users.noreply.github.com>
2026-08-27 13:07:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Isi setelah membuat project Supabase.
 // Supabase URL dan anon/public key aman ditaruh di frontend.
 // JANGAN pernah menaruh service-role key di file ini.
-window.SUPABASE_URL = "";
-window.SUPABASE_ANON_KEY = "";
+window.SUPABASE_URL = "https://aoylogtcujtemygubjho.supabase.co/rest/v1/";
+window.SUPABASE_ANON_KEY = "sb_publishable_buDWQv9mIH2h8gAyC8LuIQ_XwCdIYOJ";
